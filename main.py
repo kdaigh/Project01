adb0a000a6e944f88a117df15702c904117351ca
cmeyer <artgirlie26@gmail.com>
2018-09-05 18:49:59 -0500

    added the gui functionality so that the user can input size of board, buttons dont do anything yet. Started making it object oriented

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import math
-print ("Welcome to Minesweeper!")
-print ("Please, chose from the menu:")
-while True:
-    print("""1. Play the Game
-             2. Quit""")
-    choice = input(": ")
-    if choice == 1 :
-        print ("Please Enter the board size, it should be at least 2")
-        boardSize = input(": ")
-        print("Awsome!")
-        minMines = 1
-        minesMax = sqrt (boardSize) - 1
-    else:
-        break
+from Tkinter import *
+window=Tk()
+
+class MainWindow():
+
+    rows = raw_input("How many rows?")
+    cols = raw_input("How many cols?")
+
+    def makeGrid(rows, cols):
+        rows=int(rows)
+        cols=int(cols)
+        for i in range(0,rows):
+            for j in range(0,cols):
+                btn1 = Button(window, text=i)
+                btn1.grid(column = i, row = j, sticky=NSEW)
+
+    window.title("Welcome to Mine Sweeper")
+
+    window.geometry('1000x2000')
+
+    lbl = Label(window, text="Hello")
+
+    lbl.grid(row=0, column=0)
+
+    makeGrid(rows,cols)
+
+    window.mainloop()
