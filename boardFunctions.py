1a29baf1fdccf3430eb400ce4fd5f22b34b44d1c
cmeyer <artgirlie26@gmail.com>
2018-09-07 09:52:14 -0500

    I didnt change anything

--- boardFunctions.py
+++ boardFunctions.py
@@ -3,7 +3,6 @@ class BoardFunctions():
             rows=int(rows)
             cols=int(cols)
             grid = [[0 for x in range(cols+2)] for y in range(rows+2)]
-            btn1 = 0
             for i in range(0,rows+2):
                 for j in range(0,cols+2):
                     if(i==0 and j==0 or i==0 and j==1 or i==1 and j==0
