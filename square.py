9305368b49de2dfd7455c5d0f3e0689c63f34455
Kristi Daigh <kdaigh20@gmail.com>
2018-09-07 10:02:52 -0500

    square.py: basic definition complete

--- square.py
+++ square.py
@@ -2,3 +2,11 @@
 # Project: Minesweeper
 # Author: Kristi Daigh
 # Created: 09/07/18 09:00AM
+
+
+class Square:
+    def __init__(self):
+        self.is_mine = False
+        self.is_flagged = False
+        self.is_revealed = False
+        self.num_adj_mines = 0
