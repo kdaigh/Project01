db5e966613980785ae3619425f0fcef96f80c1b0
Ayah Alkhatib <ayah.alkhatib90@gmail.com>
2018-09-07 11:11:47 -0500

    Ayah: Function gameMenu added to the class boardFunction

--- boardFunctions.py
+++ boardFunctions.py
@@ -25,3 +25,21 @@ class BoardFunctions():
                     print grid[i][j],
                 print
 
+    def gameMenu ():
+        print ("Welcome to Minesweeper!")
+        print ("Please, chose from the menu:")
+        while True:    
+            print("""1. Play the Game
+            2. Quit""")
+            choice = int (input(": "))    
+            if choice == 1 :
+                print ("Please Enter the board size, it should be at least 2, and maximum 15")
+                boardSize = int (input(": "))
+                print ("Awsome! Let the fun begin!")
+                minMines = 1
+                maxMines = (boardSize)**2 - 1
+                print ("Enter the number of mines, it should be between 1 and " + str (maxMines))
+                minesNum = int (input ())
+
+            else:
+	        break
