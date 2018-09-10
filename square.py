46067a4a649575dc37a32718c1eb9306a33dfb47
Kristi Daigh <kdaigh20@gmail.com>
2018-09-10 12:47:12 -0500

    square.py: Added/updated comment structure

--- square.py
+++ square.py
@@ -1,20 +1,33 @@
-# Square Object
-# Project: Minesweeper
-# Author: Kristi Daigh
-# Created: 09/07/18 09:00AM
+## @package square
+#  Source file for the square object
+#
+#  Project: Minesweeper
+#  Author: Kristi Daigh
+#  Created: 09/07/18
+#  Completed:
 
 
 class Square:
     maybe = 5
 
+    ## Constructor
     def __init__(self):
         self.x = 0
         self.y = 0
+        ## @var is_mine
+        #  mine status flag
         self.is_mine = False
+        ## @var is_flagged
+        #  flag status flag
         self.is_flagged = False
+        ## @var is_revealed
+        #  reveal status flag
         self.is_revealed = True
+        ## @var num_adj_mines
+        #  tracks number of adjacent mines
         self.num_adj_mines = 0
 
+    ## Prints the square based on properties
     def print_square(self):
         if not self.is_revealed:
             if self.is_flagged:
