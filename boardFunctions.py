3dd39d1c04497cc578d63613824af19ccc48d2cb
Ayah Alkhatib <ayah.alkhatib90@gmail.com>
2018-09-12 02:57:18 +0100

    merging with master

--- boardFunctions.py
+++ boardFunctions.py
@@ -1,4 +1,3 @@
-
 #boardFunctions class
 #project: mineSweeper
 #author: Clare Meyer
@@ -13,28 +12,6 @@ class BoardFunctions():
         self.boardSize = 0
         self.mines_num = 0
 
-<<<<<<< HEAD
-    # precondition: no grid has been generated
-    # postcondition: grid generated
-    # returns: none
-    def make_grid(self,rows,cols):
-            rows=int(rows)
-            cols=int(cols)
-            grid = [[0 for x in range(cols)] for y in range(rows)]
-            for i in range(0,rows):
-                for j in range(0,cols):
-||||||| merged common ancestors
-
-    #precondition: no grid has been generated
-    #postcondition: grid generated
-    #returns: none
-    def make_grid(self,rows,cols):
-            rows=int(rows)
-            cols=int(cols)
-            grid = [[0 for x in range(cols)] for y in range(rows)]
-            for i in range(0,rows):
-                for j in range(0,cols):
-=======
 
     #@precondition: no grid has been generated
     #@postcondition: grid generated
@@ -45,7 +22,6 @@ class BoardFunctions():
             grid = [[0 for x in range(size)] for y in range(size)]
             for i in range(0,size):
                 for j in range(0,size):
->>>>>>> 2667289d32c4ce646e3d8c168d382748eeccc725
                     grid[i][j] = Square()
             return(grid)
 
@@ -70,28 +46,6 @@ class BoardFunctions():
                 grid[i][j].print_square()
             print('\n', end=' ')
 
-<<<<<<< HEAD
-    # precondition:grid does not have formatting
-    # postcondition: grid is printed to look nice for the user
-    # returns: none
-    def print_board(self,rows,cols,main_grid):
-            rows=int(rows)
-            cols=int(cols)
-            grid = [[0 for x in range(cols+2)] for y in range(rows+2)]
-            for i in range(0,rows+2):
-                for j in range(0,cols+2):
-||||||| merged common ancestors
-
-    # precondition:grid does not have formatting
-    # postcondition: grid is printed to look nice for the user
-    # returns: none
-    def print_board(self,rows,cols,main_grid):
-            rows=int(rows)
-            cols=int(cols)
-            grid = [[0 for x in range(cols+2)] for y in range(rows+2)]
-            for i in range(0,rows+2):
-                for j in range(0,cols+2):
-=======
 
     #@precondition:grid does not have formatting
     #@postcondition: grid is printed to look nice for the user
@@ -102,51 +56,57 @@ class BoardFunctions():
             grid = [[0 for x in range(size+2)] for y in range(size+2)]
             for i in range(0,size+2):
                 for j in range(0,size+2):
->>>>>>> 2667289d32c4ce646e3d8c168d382748eeccc725
                     if(i==0 and j==0 or i==0 and j==1 or i==1 and j==0
                     or i==1 and j==1):
                         grid[i][j]=" "
                     elif(j == 0):
-                        grid[i][j]= i-1
+                        grid[i][j]=i-2
                     elif(i == 0):
-                        grid[i][j]=j-1
+                        grid[i][j]=j-2
                     elif(j == 1):
                         grid[i][j]="|"
                     elif(i==1):
-                        grid[i][j]="~"
+                        grid[i][j]="--"
                     else:
                         grid[i][j] = main_grid[i-2][j-2]
 
             for i in range(0, size+2):
                 for j in range(0, size+2):
                     if(i==0 or i==1 or j==0 or j==1):
-                        print(grid[i][j], end=' ')
+                        if((i==1 and j==0)):
+                            print(grid[i][j], end=' ')
+                        elif((i==0 and j==0) or (i==0 and j==1)or (i==1 and j==1)):
+                            print((str(grid[i][j]).ljust(2)),end=' ')
+                        elif(i==0 or j==0):
+                            print((str(grid[i][j]).zfill(2)),end=' ')
+                        else:
+                            print(str(grid[i][j]).ljust(2), end=' ')
                     else:
                         grid[i][j].print_square()
                 print('\n', end=' ')
 
-    def count_nearby_mines(self, x, y):
-        adj_mine_counter = 0
-        if Square(x + 1, y).is_mine == True:
-            adj_mine_counter += 1
-        if Square(x + 1, y + 1).is_mine == True:
-            adj_mine_counter += 1
-        if Square(x + 1, y - 1).is_mine == True:
-            adj_mine_counter += 1
-        if Square(x, y + 1).is_mine == True:
-            adj_mine_counter += 1
-        if Square(x, y - 1).is_mine == True:
-            adj_mine_counter += 1
-        if Square(x - 1, y).is_mine == True:
-            adj_mine_counter += 1
-        if Square(x - 1, y + 1).is_mine == True:
-            adj_mine_counter += 1
-        if Square(x - 1, y - 1).is_mine == True:
-            adj_mine_counter += 1
-        Square(x, y).num_adj_mines = adj_mine_counter
-
-
-    def mine_check(self):
-        for w in range(0, self.boardSize):
-            for z in range(0, self.boardSize):
-                self.count_nearby_mines(Square.x, Square.y)
+    # def count_nearby_mines(self, x, y):
+    #     adj_mine_counter = 0
+    #     if Square(x + 1, y).is_mine == True:
+    #         adj_mine_counter += 1
+    #     if Square(x + 1, y + 1).is_mine == True:
+    #         adj_mine_counter += 1
+    #     if Square(x + 1, y - 1).is_mine == True:
+    #         adj_mine_counter += 1
+    #     if Square(x, y + 1).is_mine == True:
+    #         adj_mine_counter += 1
+    #     if Square(x, y - 1).is_mine == True:
+    #         adj_mine_counter += 1
+    #     if Square(x - 1, y).is_mine == True:
+    #         adj_mine_counter += 1
+    #     if Square(x - 1, y + 1).is_mine == True:
+    #         adj_mine_counter += 1
+    #     if Square(x - 1, y - 1).is_mine == True:
+    #         adj_mine_counter += 1
+    #     Square(x, y).num_adj_mines = adj_mine_counter
+    #
+    #
+    # def mine_check(self):
+    #     for w in range(0, boardSize):
+    #         for z in range(0, boardSize):
+    #             count_nearby_mines(Square.x, Square.y)
