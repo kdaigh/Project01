be420ea6f7b1fb5cb2138fce6e405d69e8ec8c63
Kristi Daigh <kdaigh20@gmail.com>
2018-09-13 10:47:12 -0500

    UserInteraction.py: Separated play() into setup() and play() functions. Resolved issues with num_flags variable. Added comments.

--- UserInteraction.py
+++ UserInteraction.py
@@ -80,52 +80,60 @@ class UserInteraction:
         else:
             return 0
 
-    def play(self):
+    ## Generates board with user input for mines and size
+    #  @author: Ethan
+    def setup(self):
 
         print("Enter board size, between 2 and 15")
         self.size = int(input())
-        max_mines = self.size ** 2 - 1
+        max_mines = self.size * self.size - 1
         print("Enter the number of mines, between 1 and " + str(max_mines))
         self.mines = int(input())
 
-        numFlags = self.mines
+        self.num_flags = self.mines
 
         self.grid = self.myBoard.make_grid(self.size)
-        self.myBoard.generate_mines(self.size,self.grid)
+        self.myBoard.generate_mines(self.mines, self.size, self.grid)
+        self.myBoard.mine_check(self.size, self.grid)
 
-        while(self.game_over == False):
+
+    ## Takes coordinates from user and handles input
+    #  @pre: Board has been setup
+    #  @author: Ethan
+    def play(self):
+
+        while not self.game_over:
             self.myBoard.print_board(self.size, self.grid)
-            print("Number of flags: %s" % numFlags)
-            user_x = int(input("Enter an Y coordinate: "))
-            user_y = int(input("Enter a X coordinate: "))
-            user_choice = input("Enter an action flag [f], reveal [r], unflag [n]: ")
-            if user_x > self.size or user_y > self.size:
+            print("Number of flags: %s" % self.num_flags)
+            x = int(input("Enter an Y coordinate: "))
+            y = int(input("Enter a X coordinate: "))
+            choice = input("Enter an action flag [f], reveal [r], unflag [n]: ")
+            if x > self.size or y > self.size:
                 print("Invalid try again")
-            elif self.grid[user_x][user_y].is_flagged == False and user_choice == "n":
+            elif not self.grid[x][y].is_flagged and choice == "n":
                 print("Invalid try again")
-            elif self.grid[user_x][user_y].is_flagged == False and numFlags == 0 and user_choice == "f":
+            elif not self.grid[x][y].is_flagged and self.num_flags == 0 and choice == "f":
                 print("Out of flags. Try again.")
-            elif self.grid[user_x][user_y].is_flagged == False and user_choice == "f":
-                self.grid[user_x][user_y].is_flagged = True
-                numFlags -= 1
+            elif not self.grid[x][y].is_flagged and choice == "f":
+                self.grid[x][y].is_flagged = True
+                self.num_flags -= 1
                 self.check_win()
-            elif self.grid[user_x][user_y].is_flagged == True and user_choice == "f":
+            elif self.grid[x][y].is_flagged and choice == "f":
                 print("Space is already flagged. Try again.")
-            elif self.grid[user_x][user_y].is_revealed == True and user_choice == "f":
+            elif self.grid[x][y].is_revealed and choice == "f":
                 print("You can't flag a revealed space. Try again.")
-            elif self.grid[user_x][user_y].is_revealed == True and user_choice == "n":
+            elif self.grid[x][y].is_revealed and choice == "n":
                 print("You can't unflag a revealed space. Try again.")
-            elif self.grid[user_x][user_y].is_flagged == True and user_choice == "n":
-                self.grid[user_x][user_y].is_flagged = False
-                numFlags += 1
+            elif self.grid[x][y].is_flagged and choice == "n":
+                self.grid[x][y].is_flagged = False
+                self.num_flags += 1
             #Testing to see if is_revealed is being switched to true
-            elif self.grid[user_x][user_y].is_revealed == True and self.grid[user_x][user_y].is_mine == False and user_choice == "r":
+            elif self.grid[x][y].is_revealed and not self.grid[x][y].is_mine and choice == "r":
                 print("Space is already revealed. Try again.")
-            elif self.grid[user_x][user_y].is_flagged == True and user_choice == "r":
+            elif self.grid[x][y].is_flagged and choice == "r":
                 print("You can't reveal a flagged space. Unflag before guessing this space or guess a different space.")
-            elif self.grid[user_x][user_y].is_mine == True and user_choice == "r":
+            elif self.grid[x][y].is_mine and choice == "r":
                 print("Game Over")
                 self.game_over = True
-                self.play_again()
             else:
-                self.reveal(user_x, user_y)
+                self.reveal(x, y)
